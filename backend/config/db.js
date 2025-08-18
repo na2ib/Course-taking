5c937da8eaceb585843cad366dff2fc15996d2c3
Nazib <94198962+na2ib@users.noreply.github.com>
2025-08-18 18:45:07 +0600

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,18 +1,21 @@
-import mysql from 'mysql'
+import mysql from 'mysql';
+import { config } from "dotenv";
+
+config(); // This loads your .env file for local development
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'xyz'
-})
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
 
 db.connect((err) => {
     if (err) {
-        console.log('Db connection error');
-        return
+        console.log("Db connection error");
+        return;
     }
-    console.log('Db Connected Successfully');
-})
+    console.log("Db Connected Successfully");
+});
 
-export default db
+export default db;
