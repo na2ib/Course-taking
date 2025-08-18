e52b670572aa70a316bb3b7631e32c2f43025b9d
Nazib <94198962+na2ib@users.noreply.github.com>
2025-08-18 16:07:51 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,8 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port ? port : '3000'}`);
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
